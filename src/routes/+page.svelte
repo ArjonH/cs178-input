352d586d23e079886eb1ff20990cfaf92f67472a
ArjonH <hevar@dhcp-10-250-174-164.harvard.edu>
2024-02-04 18:06:38 -0500

    Revert "google calendar integration"
    This reverts commit d7288a827a34fa7c8634ac7b8cfcb09ad3cd1c8d.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,107 +30,33 @@
         ec = ec.unselect()
     }
 
-    const CLIENT_ID = '669688591392-rhdn9ebnpq24fc3l08m45ud6tbh8rf4j.apps.googleusercontent.com';
-    const API_KEY = 'AIzaSyDDfNxkzJppzzegvfAr9WGc-Y0RzlquirU';
+//     const start = async () => {
+//     // Initializes the client with the API key and the Translate API.
+//     // @ts-ignore
+//     gapi.client.init({
+//       'apiKey': 'AIzaSyDDfNxkzJppzzegvfAr9WGc-Y0RzlquirU',
+//       'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+//     }).then(function() {
+//       // Executes an API request, and returns a Promise.
+//       // The method name `language.translations.list` comes from the API discovery.
+//       return gapi.client.language.translations.list({
+//         q: 'hello world',
+//         source: 'en',
+//         target: 'de',
+//       });
+//     }).then(function(response) {
+//       console.log(response.result.data.translations[0].translatedText);
+//     }, function(reason) {
+//       console.log('Error: ' + reason.result.error.message);
+//     });
+//   };
 
-    const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
-    const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
-    let tokenClient;
-    let gapiInited = false;
-    let gisInited = false;
-
-    function gapiLoaded() {
-        gapi.load('client', initializeGapiClient);
-    }
-    async function initializeGapiClient() {
-        await gapi.client.init({
-          apiKey: API_KEY,
-          discoveryDocs: [DISCOVERY_DOC],
-        });
-        gapiInited = true;
-        maybeEnableButtons();
-        console.log("gapi")
-    }
-    function gisLoaded() {
-        tokenClient = google.accounts.oauth2.initTokenClient({
-          client_id: CLIENT_ID,
-          scope: SCOPES,
-          callback: '', // defined later
-        });
-        gisInited = true;
-        maybeEnableButtons();
-    }
-
-    function maybeEnableButtons() {
-        console.log("enabled")
-        if (gapiInited && gisInited) {
-          document.getElementById('authorize_button').style.visibility = 'visible';
-        }
-    }
-
-    function handleAuthClick() {
-        console.log("auth")
-        tokenClient.callback = async (resp) => {
-          if (resp.error !== undefined) {
-            throw (resp);
-          }
-        //   document.getElementById('signout_button').style.visibility = 'visible';
-        //   document.getElementById('authorize_button').innerText = 'Refresh';
-          await listUpcomingEvents();
-        };
-        if (gapi.client.getToken() === null) {
-          // Prompt the user to select a Google Account and ask for consent to share their data
-          // when establishing a new session.
-          tokenClient.requestAccessToken({prompt: 'consent'});
-        } else {
-          // Skip display of account chooser and consent dialog for an existing session.
-          tokenClient.requestAccessToken({prompt: ''});
-        }
-    }
-    function handleSignoutClick() {
-        const token = gapi.client.getToken();
-        if (token !== null) {
-          google.accounts.oauth2.revoke(token.access_token);
-          gapi.client.setToken('');
-          document.getElementById('content').innerText = '';
-          document.getElementById('authorize_button').innerText = 'Authorize';
-          document.getElementById('signout_button').style.visibility = 'hidden';
-        }
-    }
-    async function listUpcomingEvents() {
-        let response;
-        try {
-          const request = {
-            'calendarId': 'primary',
-            'timeMin': (new Date()).toISOString(),
-            'showDeleted': false,
-            'singleEvents': true,
-            'maxResults': 10,
-            'orderBy': 'startTime',
-          };
-          response = await gapi.client.calendar.events.list(request);
-        } catch (err) {
-          document.getElementById('content').innerText = err.message;
-          return;
-        }
-        const events = response.result.items;
-        if (!events || events.length == 0) {
-          document.getElementById('content').innerText = 'No events found.';
-          return;
-        }
-        // Flatten to string to display
-        const output = events.reduce(
-            (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
-            'Events:\n');
-        console.log(output)
-    }
-
+//   const initializeGapi = async () => {
+//     gapi.load('client', start);
+//   }
 </script>
 
-<svelte:head>
-    <script async defer src="https://apis.google.com/js/api.js" on:load={gapiLoaded()}></script>
-    <script async defer src="https://accounts.google.com/gsi/client" on:load={gisLoaded()}></script>
-</svelte:head>
-<button id="authorize_button" on:click={() => handleAuthClick()}>Authorize</button>
-<button id="signout_button" on:click={() => handleSignoutClick()}>Sign Out</button>
+<!-- <svelte:head>
+    <script src="https://apis.google.com/js/api.js" on:load={initializeGapi}></script>
+</svelte:head> -->
 <Calendar bind:this={ec} {plugins} {options} />
